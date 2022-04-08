9cc6d69266c8d08d9a8c0606cb26577b557481f8
Brent Williams <brent.williams@ddincmail.org>
2022-04-08 11:12:11 -0400

    Added documentation to the sketch.js file

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,73 @@
+//Imports
 import p5 from 'p5';
+import ml5 from 'ml5';
+import Puffin from './images/puffin.jpg';
+import Penguin from './images/penguin.jpg';
+import Toucan from './images/toucan.jpg';
 
-const App = (sketch) => 
+//Image Declarations
+let puffin;
+let toucan;
+let penguin;
+
+/**
+* App is a p5 construct declaration of a new instance of p5. 
+* p5 uses a lot of ES6 related syntax such as arrow functions, promises and async functions.
+* @author Brent Williams <brent.williams@ddincmail.org>
+*/
+
+const App = (App) => 
 {
-    sketch.setup = () =>
+    //Mobile Net ML Model Declaration
+    App.mobileNet = ml5.imageClassifier('MobileNet', MobileNetReady);
+
+    /**
+    * p5JS Initialization Function -- fn() setup is ran automatically by p5JS.
+    */
+    
+    App.setup = () =>
     {
-        sketch.createCanvas(640,400);
+        App.createCanvas(400,400);
+        toucan = App.createImg(Toucan, () => 
+        {
+            App.image(toucan, 0, 0, App.width, App.height);
+        });
+        toucan.hide();
+        App.background(0);
     }
 
-    sketch.draw = () => 
+    /**
+    * Function Callback when Mobile Net ML Model is ready.
+    *
+    * @return {log} Returns logs ml5 version # and statement saying its ready.
+    */
+    
+    function MobileNetReady() 
     {
-        sketch.background(0);
+        console.group('ml5 Information');
+        console.log(`ml5 Version: ${ml5.version}`);
+        console.log('MobileNet is ready.');
+
+        App.mobileNet.classify(toucan,ResultsReturned);
+    }
+
+    /**
+    * Returns ML Models prediction results.
+    *
+    * @param {err} Any errors that arise from the prediction.
+    * @param {results} Predicted Results.
+    */
+    
+    function ResultsReturned(err, results) 
+    {
+        if(err)
+        {
+            console.error(error);
+        }
+        else
+        {
+            console.log(results);
+        }
     }
 }
 
